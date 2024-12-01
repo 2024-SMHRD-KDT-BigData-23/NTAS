ea3e5444e92fc422d8776a6ce0c58aff021ee251
이지훈 <155136709+dlwlgnsrhy@users.noreply.github.com>
2024-12-01 15:47:13 +0900

    Update JWTFilter.java

--- backend/src/main/java/com/smhrd/namnam/jwt/JWTFilter.java
+++ backend/src/main/java/com/smhrd/namnam/jwt/JWTFilter.java
@@ -28,32 +28,24 @@ public class JWTFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String authorization = request.getHeader("Authorization");
-
         if (authorization == null || !authorization.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
             return;
         }
-
         String token = authorization.split(" ")[1];
-
         if (jwtUtil.isExpired(token)) {
             filterChain.doFilter(request, response);
             return;
         }
-
         String username = jwtUtil.getUsername(token);
         String role = jwtUtil.getRole(token);
-
         Optional<StaffInfo> staffInfoOptional = userService.findById(username);
-
         if (staffInfoOptional.isPresent()) {
             StaffInfo staffInfo = staffInfoOptional.get();
             CustomUserDetails customUserDetails = new CustomUserDetails(staffInfo);
-
             Authentication authToken = new UsernamePasswordAuthenticationToken(customUserDetails, null, customUserDetails.getAuthorities());
             SecurityContextHolder.getContext().setAuthentication(authToken);
         }
-
         filterChain.doFilter(request, response);
     }
 }

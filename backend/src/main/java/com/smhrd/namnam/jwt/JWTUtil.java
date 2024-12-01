c5cf4ab98a48ec1d0ab48303274b7ab65be4006b
이지훈 <155136709+dlwlgnsrhy@users.noreply.github.com>
2024-12-01 15:45:10 +0900

    Update JWTUtil.java

--- backend/src/main/java/com/smhrd/namnam/jwt/JWTUtil.java
+++ backend/src/main/java/com/smhrd/namnam/jwt/JWTUtil.java
@@ -12,45 +12,11 @@ import java.util.Date;
 
 @Component
 public class JWTUtil {
-
     private final SecretKey secretKey;
-
     public JWTUtil(@Value("${jwt.secret}") String secret) {
-
         this.secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
         //secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
     }
-
-    public String getUsername(String token) {
-        return Jwts.parser()
-                .verifyWith(secretKey)
-                .build()
-                .parseSignedClaims(token)
-                .getPayload()
-                .get("username", String.class);
-    }
-
-    public String getRole(String token) {
-
-        return Jwts.parser()
-                .verifyWith(secretKey)
-                .build()
-                .parseSignedClaims(token)
-                .getPayload()
-                .get("role", String.class);
-    }
-
-    public Boolean isExpired(String token) {
-
-        return Jwts.parser()
-                .verifyWith(secretKey)
-                .build()
-                .parseSignedClaims(token)
-                .getPayload()
-                .getExpiration()
-                .before(new Date());
-    }
-
     public String createJwt(String username, String role, Long expiredMs) {
         return Jwts.builder()
                 .claim("username", username)
@@ -60,13 +26,37 @@ public class JWTUtil {
                 .signWith(secretKey)
                 .compact();
     }
+    public String getUsername(String token) {
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .get("username", String.class);
+    }
+    public String getRole(String token) {
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .get("role", String.class);
+    }
+    public Boolean isExpired(String token) {
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getExpiration()
+                .before(new Date());
+    }
 
     public void invalidateToken(String token) {
         System.out.println("Invalidating token: " + token);
-        // 토큰 무효화 로직 추가 (예: 블랙리스트에 추가)
-        // 현재 예시로는 아무 작업도 하지 않습니다.
+        // 토큰 무효화 로직 추가
+        // 현재는 아무 작업도 하지 않습니다.
     }
-
     public boolean validateToken(String token) {
         try {
             System.out.println("Validating token: " + token);
